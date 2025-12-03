a9f0a93367c0b37482ecc6d8672b92767de60e92
Apander <Apander204@gmail.com>
2025-12-03 22:38:06 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT;
+
+const baseRouter = require('./routes/baseRouter')
+const categoryRouter = require('./routes/categoryRouter');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", baseRouter);
+app.use("/category/", categoryRouter);
+
+app.listen(PORT || 3000, () => {
+    console.log("Server Listening on port ", PORT);
+});
